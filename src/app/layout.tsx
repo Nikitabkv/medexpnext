42ca7413881b547436a37c97289d5d094e92c435
Nikita Bakumenko <n.bakumenko@numedy.com>
2024-08-16 16:52:03 +0300

    feat: add adaptive for footer popups

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={roboto.className}>
-        <Header />
+        {/*<Header />*/}
         <main>
           {children}
         </main>
